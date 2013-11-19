0f2aeb7b3c877450336ab14c49fde68df8bcb512
Stronger Shen <strongershen@gmail.com>
2013-11-19 15:47:53 +0800

    modify remark date and in XP801

--- LocalNotificationSchedule/ViewController.h
+++ LocalNotificationSchedule/ViewController.h
@@ -2,7 +2,7 @@
 //  ViewController.h
 //  LocalNotificationSchedule
 //
-//  Created by Stronger Shen on 13/10/22.
+//  Created by Stronger Shen on 2013/10/22 in XP801
 //  Copyright (c) 2013年 MobileIT. All rights reserved.
 //
 
